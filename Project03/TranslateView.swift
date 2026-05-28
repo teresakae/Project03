6e9d2ced189b446885c7ea96ddd180361ba49aea
Luisa <luisahaning.t@gmail.com>
2026-05-28 22:24:38 +0800

    text styles on manual translate page

--- Project03/TranslateView.swift
+++ Project03/TranslateView.swift
@@ -12,19 +12,25 @@ struct TranslateView: View {
             VStack (alignment: .leading) {
                 VStack (alignment: .leading){
                     Text("English (EN)")
+                        .font(.headline)
+                        .fontWeight(.semibold)
                     Text("I can't eat pork and shrimp.")
+                        .font(.subheadline)
                 }
                 .foregroundStyle(Color.primary)
                 Divider()
                 VStack (alignment: .leading){
                     HStack{
                         Text("Indonesian (IDN)")
+                            .font(.headline)
+                            .fontWeight(.semibold)
                         Spacer()
                         Image(systemName: "speaker.wave.2.circle.fill")
                     }
                     
                     HStack{
                         Text("Saya tidak bisa makan daging babi dan udang")
+                            .font(.subheadline)
                         //                    Spacer()
                         //                    Image(systemName: "speaker.wave.2.circle.fill")
                     }
